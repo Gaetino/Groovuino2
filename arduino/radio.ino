37be1080e8df6c8694fc394967f19f91a47acf39
Gaetino <gaith26740@yahoo.fr>
2015-12-16 00:23:37 +0100

    Update radio.ino

--- arduino/radio.ino
+++ arduino/radio.ino
@@ -2,17 +2,21 @@
 #include <SPI.h>
 #include "LCD_Functions.h"
 
-const int pin_button = 12;
+int lignes = 3;
+int nDevices = 0;
+const int pin_button = 14;
 const int pin_A = 8;  // pin 12
 const int pin_B = 4;  // pin 11
 unsigned char encoder_A;
 unsigned char encoder_B;
 unsigned char encoder_A_prev=0;
 boolean butstate = false;
+int numscreen = 0; // 0 = menu principal
+                   // 1 = liste de modules dans "set module"
 
 int i2c_address_but8LED[8] = {0,0,0,0,0,0,0,0};
 
-int mode = 1;
+int menu = 1;
 
 void setup()
 {
@@ -31,17 +35,16 @@ void setup()
   
   clearDisplay(WHITE);
   updateDisplay();
-  setStr("start scan...",0,0,BLACK);
+  setStr("START SCAN...",0,0,BLACK);
   updateDisplay();
-  scan_i2c()
+  scan_i2c();
+  clearDisplay(WHITE);
+  setStr("END SCAN...",0,0,BLACK);
+  updateDisplay();
+  delay(1000);
   
   clearDisplay(WHITE);
-  updateDisplay();
-  setStr("MODE 1", 2, 2, BLACK);
-  setStr("MODE 2", 2, 17, BLACK);
-  setStr("MODE 3", 2, 32, BLACK);
-  setRect(0, 0, 40, 12, 0, BLACK);
-  updateDisplay();
+  main_menu();
 }
 
 void main_menu()
@@ -51,15 +54,44 @@ void main_menu()
   setStr("LOAD CONFIG", 2, 2, BLACK);
   setStr("SET MODULE", 2, 17, BLACK);
   setStr("SCAN MODULES", 2, 32, BLACK);
-  setRect(0, 0, 40, 12, 0, BLACK);
+  setRect(0, 0, 80, 12, 0, BLACK);
+  updateDisplay();
+}
+
+void mode_select()
+{
+  clearDisplay(WHITE);
+  updateDisplay();
+  setStr("RETOUR", 2, 2, BLACK);
+  setStr("MODE 1", 2, 17, BLACK);
+  setStr("MODE 2", 2, 32, BLACK);
+  setRect(0, 0, 80, 12, 0, BLACK);
+  updateDisplay();
+}
+
+void list_module()
+{
+  clearDisplay(WHITE);
+  updateDisplay();
+  setStr("RETOUR", 2, 2, BLACK);
+  if(nDevices > 0) setStr("MODULE 1", 2, 17, BLACK);
+  if(nDevices > 1) setStr("MODULE 2", 2, 32, BLACK);
+  if(nDevices > 0) setRect(0, 0, 80, 12, 0, BLACK);
+  setRect(0, 0, 80, 12, 0, BLACK);
   updateDisplay();
 }
 
 void scan_i2c()
 {
-  int nDevices = 0;
+  
+  byte error, address;
   for(address = 1; address < 16; address++ ) 
   {
+    clearDisplay(WHITE);
+    updateDisplay();
+    setStr("MODULE",0,0,BLACK);
+    setChar(48+address,10,10,BLACK);
+    updateDisplay();
     // The i2c_scanner uses the return value of
     // the Write.endTransmisstion to see if
     // a device did acknowledge to the address.
@@ -71,8 +103,8 @@ void scan_i2c()
       i2c_address_but8LED[nDevices] = address;
       clearDisplay(WHITE);
       updateDisplay();
-      setStr("found module ",0,0,BLACK);
-      setStr(address,10,10,BLACK);
+      setStr("FOUND MODULE ",0,0,BLACK);
+      setChar(48+address,10,10,BLACK);
       updateDisplay();
       nDevices++;
       delay(2000);
@@ -81,18 +113,20 @@ void scan_i2c()
     {
       clearDisplay(WHITE);
       updateDisplay();
-      setStr("error module ",0,0,BLACK);
-      setStr(address,10,10,BLACK);
+      setStr("ERROR MODULE ",0,0,BLACK);
+      setChar(48+address,10,10,BLACK);
       updateDisplay();
       delay(2000);
-    }    
+    }   
+    delay(200); 
   }
   if (nDevices == 0)
   {
     clearDisplay(WHITE);
     updateDisplay();
-    setStr("no module",0,0,BLACK);
+    setStr("NO MODULE",0,0,BLACK);
     updateDisplay();
+    delay(1000);
   }
 }
 
@@ -105,35 +139,35 @@ void update_menu(int mode)
 
 void select_menu(int sens)
 {
-  if(sens==1 && mode<3)
+  if(sens==1 && menu<lignes)
   {
-    mode++;
-    if(mode==2) 
+    menu++;
+    if(menu==2) 
     {
-      setRect(0, 0, 40, 12, 0, WHITE);
-      setRect(0, 15, 40, 27, 0, BLACK);
+      setRect(0, 0, 80, 12, 0, WHITE);
+      setRect(0, 15, 80, 27, 0, BLACK);
       updateDisplay();
     }
-    if(mode==3) 
+    if(menu==3) 
     {
-      setRect(0, 15, 40, 27, 0, WHITE);
-      setRect(0, 30, 40, 42, 0, BLACK);
+      setRect(0, 15, 80, 27, 0, WHITE);
+      setRect(0, 30, 80, 42, 0, BLACK);
       updateDisplay();
     }
   }
-  if(sens==-1 && mode>1)
+  if(sens==-1 && menu>1)
   {
-    mode--;
-    if(mode==2) 
+    menu--;
+    if(menu==2) 
     {
-      setRect(0, 30, 40, 42, 0, WHITE);
-      setRect(0, 15, 40, 27, 0, BLACK);
+      setRect(0, 30, 80, 42, 0, WHITE);
+      setRect(0, 15, 80, 27, 0, BLACK);
       updateDisplay();
     }
-    if(mode==1) 
+    if(menu==1) 
     {
-      setRect(0, 15, 40, 27, 0, WHITE);
-      setRect(0, 0, 40, 12, 0, BLACK);
+      setRect(0, 15, 80, 27, 0, WHITE);
+      setRect(0, 0, 80, 12, 0, BLACK);
       updateDisplay();
     }
   }
@@ -141,36 +175,104 @@ void select_menu(int sens)
 
 void loop()
 {
-  Wire.requestFrom(1, 3);    
-  delay(2);
-  
-  byte changed = 0;
-  byte param_number = 0;
-  byte param_value = 0; 
-  
-  while(Wire.available())    // slave may send less than requested
-  { 
-    changed = Wire.read();
-    param_number = Wire.read();
-    param_value = Wire.read();
-  }
-  
-  if(changed > 0) 
+  for(int i = 0; i<nDevices; i++)
   {
-    Serial1.write(255);
-    Serial1.write()
-    Serial1.write(param_number);
-    Serial1.write(param_value);
-  }
+    Wire.requestFrom(i2c_address_but8LED[i], 3);    
+    delay(2);
   
-  delay(1);
+    byte changed = 0;
+    byte param_number = 0;
+    byte param_value = 0; 
+  
+    while(Wire.available())    // slave may send less than requested
+    { 
+      changed = Wire.read();
+      param_number = Wire.read();
+      param_value = Wire.read();
+    }
+  
+    if(changed > 0) 
+    {
+      Serial1.write(255);
+      Serial1.write(i2c_address_but8LED[i]);
+      Serial1.write(param_number);
+      Serial1.write(param_value);
+    }
+    delay(1);
+  }
   
   int rot = ReadRotary();
-  if(rot!=0) select_mode(rot);
-  
+  if(rot!=0) select_menu(rot);
+  if(butpressed())
+  {
+    switch(menu)
+    {
+    case 3: 
+      if(numscreen==0)
+      {
+        scan_i2c(); 
+        lignes = 3;
+        main_menu();
+      }
+      if(numscreen==2)
+      {
+        Wire.beginTransmission(1);
+        Wire.write(255);
+        Wire.write(2);
+        Wire.endTransmission();
+      }
+      if(numscreen==1)
+      {
+        lignes = 3;
+        menu = 1;
+        numscreen = 2;
+        mode_select();
+      }
+      break;
+    case 2:
+      if(numscreen==2)
+      {
+        Wire.beginTransmission(1);
+        Wire.write(255);
+        Wire.write(1);
+        Wire.endTransmission();
+      }
+      if(numscreen==1)
+      {
+        lignes = 3;
+        menu = 1;
+        numscreen = 2;
+        mode_select();
+      }
+      if(numscreen==0)
+      {
+        menu = 1;
+        numscreen = 1;
+        lignes = nDevices+1;
+        list_module();
+      }
+      
+      break;
+    case 1:
+      if(numscreen==1)
+      {
+        menu = 1;
+        numscreen = 0;
+        lignes = 3;
+        main_menu();
+      }
+      break;
+      if(numscreen==2)
+      {
+        menu = 1;
+        numscreen = 1;
+        lignes = nDevices+1;
+        list_module();
+      }
+      break;
+    }
+  }
 }
-
-
 
 int ReadRotary()
 {
@@ -190,6 +292,7 @@ boolean butpressed()
 {
   if(digitalRead(pin_button) && !butstate)
   {
+    Serial.println("pressed");
     butstate = true;
     return true;
   }
