a4fd07a928198a0f2cb585a22d762b415f7a22ec
Gaetino <gaith26740@yahoo.fr>
2016-01-01 23:36:56 +0100

    Update teensy.ino

--- arduino/teensy.ino
+++ arduino/teensy.ino
@@ -1,10 +1,12 @@
-int but8LED_num = 0;
-int but8LED_address[8] = {0,0,0,0,0,0,0,0};
+
+int but8LED_num = 2;
+int but8LED_address[8] = {0,2,0,0,0,0,0,0};
 int but8LED_mode[8] = {0,0,0,0,0,0,0,0};
 
 void setup()
 {
   Serial3.begin(9600);  // start serial for output
+  send_config();
 }
 
 void loop()
@@ -13,17 +15,32 @@ void loop()
   {
     if(Serial3.read()==255)
     {
-      if(Serial3.read()==0)
+      delay(5);
+      int test = Serial3.read();
+      if(test==0)
       {
-        Serial3.write(255);
-        Serial3.write(0);
-        for(int i = 0; i<but8LED_num; i++)
-        {
-          Serial3.write(0);
-          Serial3.write(but8LED_mode[i]);
-          Serial3.write(but8LED_address[i]);
-        }
+        Serial.println("transmit");
+        send_config();
+      }
+      if(test==1)
+      {
+        Serial.println("param receive");
+        Serial.println(Serial3.read());
+        Serial.println(Serial3.read());
+        Serial.println(Serial3.read());
       }
     }
+  }
+}
+
+void send_config()
+{
+  Serial3.write(255);
+  Serial3.write(0);
+  for(int i = 0; i<but8LED_num; i++)
+  {
+    Serial3.write(0);
+    Serial3.write(but8LED_address[i]);
+    Serial3.write(but8LED_mode[i]);
   }
 }
